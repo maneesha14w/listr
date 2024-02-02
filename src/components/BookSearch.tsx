f7eb68f2d484a73d8fc35e0b0f7f76dc22d99e11
Maneesha <maneesha14w@gmail.com>
2024-02-02 14:07:25 +0530

    feat: added pagination.

--- src/components/BookSearch.tsx
+++ src/components/BookSearch.tsx
@@ -22,36 +22,61 @@ export type Book = {
 }
 
 export const BookSearch = () => {
+	// State variables
 	const [query, setQuery] = useState("")
 	const [result, setResults] = useState<any[]>([])
 	const [isLoading, setIsLoading] = useState(false)
+	const [totalResults, setTotalResults] = useState(0)
+	const [currentPage, setCurrentPage] = useState(1)
+	const resultsPerPage = 100
 
+	//structure of the API response
 	type SearchResult = {
 		docs: any[]
 		num_found: number
 	}
 
-	const searchBooks = async () => {
+	//Async function to search books based on the query and page number.
+	const searchBooks = async (page: number = 1) => {
 		if (!query) return
 
 		setIsLoading(true)
 		try {
+			// Fetch data from Open Library API using Axios
 			const response = await axios.get<SearchResult>(
-				`https://openlibrary.org/search.json?q=${query}`,
+				`https://openlibrary.org/search.json?q=${query}&page=${page}&limit=${resultsPerPage}`,
 			)
+			// Set state variables based on API response
 			setResults(response.data.docs)
+			setTotalResults(response.data.num_found)
+			setCurrentPage(page)
 		} catch (error) {
 			console.error("Error fetching Open Library Data", error)
 		}
 		setIsLoading(false)
 	}
 
+	// Handler for navigating to the previous page
+	const prevPageHandler = () => {
+		if (currentPage > 1) {
+			searchBooks(currentPage - 1)
+		}
+	}
+	// Handler for navigating to the next page
+	const nextPageHandler = () => {
+		if (currentPage < Math.ceil(totalResults / resultsPerPage)) {
+			searchBooks(currentPage + 1)
+		}
+	}
+
+	// Handler for Enter key press in the search input
 	const handleKeyPress = (event: React.KeyboardEvent) => {
 		if (event.key === "Enter") {
 			searchBooks()
 		}
 	}
 
+	// JSX structure for rendering the component
 	return (
 		<div className=" p-4">
 			<div className="sm:max-w-xs">
@@ -82,7 +107,7 @@ export const BookSearch = () => {
 						</TableRow>
 					</TableHeader>
 					<TableBody>
-						{result.map((book, index) => (
+						{result.map((book) => (
 							<TableRow>
 								<TableCell>{book.title}</TableCell>
 								<TableCell>{book.author_name}</TableCell>
@@ -92,6 +117,25 @@ export const BookSearch = () => {
 						))}
 					</TableBody>
 				</Table>
+			</div>
+			<div className="mt-4 flex items-center justify-between">
+				<Button
+					variant={"outline"}
+					disabled={currentPage <= 1 || isLoading}
+					onClick={prevPageHandler}
+				>
+					Previous
+				</Button>
+				<span>{`Page ${currentPage}`}</span>
+				<Button
+					variant={"outline"}
+					disabled={
+						currentPage >= Math.ceil(totalResults / resultsPerPage) || isLoading
+					}
+					onClick={nextPageHandler}
+				>
+					Next
+				</Button>
 			</div>
 		</div>
 	)
