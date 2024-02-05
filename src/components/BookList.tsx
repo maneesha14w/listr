98c5b70a497907132812a65c6459ae7b67f8a874
Maneesha <maneesha14w@gmail.com>
2024-02-05 13:00:17 +0530

    feat: added other lists.

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -58,12 +58,32 @@ export const BookList = ({
 	return (
 		<div className="space-y-8 p-4">
 			<h2 className="mb-4 text-2xl font-bold">My Reading List</h2>
-			{books.filter((book) => book.status === "toRead").length > 0 && (
+			{books.filter((book) => book.status === "reading").length > 0 && (
 				<>
 					<h3 className="mb-2 text-xl font-semibold">Reading</h3>
 					<div>
 						{books
+							.filter((book) => book.status === "reading")
+							.map((book, index) => renderBookItem(book, index))}
+					</div>
+				</>
+			)}
+			{books.filter((book) => book.status === "toRead").length > 0 && (
+				<>
+					<h3 className="mb-2 text-xl font-semibold">To Read</h3>
+					<div>
+						{books
 							.filter((book) => book.status === "toRead")
+							.map((book, index) => renderBookItem(book, index))}
+					</div>
+				</>
+			)}
+			{books.filter((book) => book.status === "complete").length > 0 && (
+				<>
+					<h3 className="mb-2 text-xl font-semibold">Completed</h3>
+					<div>
+						{books
+							.filter((book) => book.status === "complete")
 							.map((book, index) => renderBookItem(book, index))}
 					</div>
 				</>
