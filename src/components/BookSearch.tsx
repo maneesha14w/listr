4acb73d90fa5b4924c51b3255c636b9987017673
Maneesha <maneesha14w@gmail.com>
2024-02-09 13:13:12 +0530

    fix: added scrollbar styles.

--- src/components/BookSearch.tsx
+++ src/components/BookSearch.tsx
@@ -158,30 +158,41 @@ export const BookSearch = () => {
 						</div>
 					)}
 				</div>
-				{query.length > 0 && result.length > 0 ? (
-					<div className="mt-4 flex items-center justify-between">
-						<Button
-							variant={"outline"}
-							disabled={currentPage <= 1 || isLoading}
-							onClick={prevPageHandler}
-						>
-							Previous
-						</Button>
-						<span>{`Page ${currentPage}`}</span>
-						<Button
-							variant={"outline"}
-							disabled={
-								currentPage >= Math.ceil(totalResults / resultsPerPage) ||
-								isLoading
-							}
-							onClick={nextPageHandler}
-						>
-							Next
-						</Button>
-					</div>
-				) : (
-					<div></div>
-				)}
+				<div
+					className="max-h-[400px] overflow-y-auto
+  [&::-webkit-scrollbar-thumb]:rounded-full
+  [&::-webkit-scrollbar-thumb]:bg-gray-300
+  dark:[&::-webkit-scrollbar-thumb]:bg-slate-500
+  [&::-webkit-scrollbar-track]:rounded-full
+  [&::-webkit-scrollbar-track]:bg-gray-100
+  dark:[&::-webkit-scrollbar-track]:bg-slate-700
+  [&::-webkit-scrollbar]:w-2"
+				>
+					{query.length > 0 && result.length > 0 ? (
+						<div className="mt-4 flex items-center justify-between">
+							<Button
+								variant={"outline"}
+								disabled={currentPage <= 1 || isLoading}
+								onClick={prevPageHandler}
+							>
+								Previous
+							</Button>
+							<span>{`Page ${currentPage}`}</span>
+							<Button
+								variant={"outline"}
+								disabled={
+									currentPage >= Math.ceil(totalResults / resultsPerPage) ||
+									isLoading
+								}
+								onClick={nextPageHandler}
+							>
+								Next
+							</Button>
+						</div>
+					) : (
+						<div></div>
+					)}
+				</div>
 			</div>
 		</div>
 	)
